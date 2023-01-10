d883954ba10c6f68b829694cb597ac03a2626e00
gueye tamsir <gueye.tamsir78@gmail.com>
2023-01-10 21:36:28 +0100

    add dotenv

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,15 @@
 class BookmarksController < ApplicationController
 
-  def edit
+  def new
+    @bookmark = Bookmark.new
+    @list = List.where(id: params[:list_id]).first
+    # @movie = Movie.find(params[:movie_id])
+  end
+
+  def destroy
     @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
 
   def create
@@ -11,7 +19,13 @@ class BookmarksController < ApplicationController
     if @bookmark.save
       redirect_to list_path(@bookmark.list)
     else
-      render "lists/show"
+      render :new, status => :unprocessable_entity
     end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/delete'
-  get 'lists/index'
+  # get 'bookmarks/new'
+  # get 'bookmarks/delete'
+  # get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   get "/lists/new", to: "lists#new", as: :new_list
   post "/lists", to: "lists#create"
   get "/lists/:id", to: "lists#show", as: :list
-  get "/lists/:id/edit", to: "bookmarks#edit", as: :edit_bookmark
+  get "/lists/:id/new", to: "bookmarks#new", as: :new_bookmark
 
 end
